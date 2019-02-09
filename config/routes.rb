f1048a15651749156bcf890ae3faa3496e5757c9
Takatomo Hattori <zgmwo9a@gmail.com>
2019-02-09 19:13:33 +0900

    2/9 commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   resources :users, only: %i(index show)
+  resources :reaction, only: %i(create)
+
+  resources :reaction, only: %i(index)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :matching, only: %i(index)
+  resources :chat, only: %i(create show)
 
 end
